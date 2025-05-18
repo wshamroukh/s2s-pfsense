979013076f617abc47d8d01483ae017756e8c477
wshamroukh <wshamroukh@gmail.com>
2025-05-18 12:44:16 +0400

    generating backup files

--- s2s-pfsense.sh
+++ s2s-pfsense.sh
@@ -32,7 +32,7 @@ az network vnet subnet create -g $rg -n $site1_fw_subnet_name --address-prefixes
 
 # create a managed disk from a vhd
 echo -e "\e[1;36mCreating $site1_vnet_name-fw managed disk from a vhd...\e[0m"
-az disk create --resource-group $rg --name $site1_vnet_name-fw --sku $storageType --location $location --size-gb 30 --source $vhdUri --os-type Linux
+az disk create --resource-group $rg --name $site1_vnet_name-fw --sku $storageType --location $location --size-gb 30 --source $vhdUri --os-type Linux -o none
 #Get the resource Id of the managed disk
 diskId=$(az disk show --name $site1_vnet_name-fw --resource-group $rg --query [id] -o tsv | tr -d '\r')
 
@@ -41,7 +41,7 @@ echo -e "\e[1;36mCreating $site1_vnet_name-fw VM...\e[0m"
 az network public-ip create -g $rg -n $site1_vnet_name-fw -l $location --allocation-method Static --sku Basic -o none
 az network nic create -g $rg -n $site1_vnet_name-fw-wan --subnet $site1_fw_subnet_name --vnet-name $site1_vnet_name --ip-forwarding true --private-ip-address 10.1.0.250 --public-ip-address $site1_vnet_name-fw -o none
 az network nic create -g $rg -n $site1_vnet_name-fw-lan --subnet $site1_vm_subnet_name --vnet-name $site1_vnet_name --ip-forwarding true --private-ip-address 10.1.1.250 -o none
-az vm create --name $site1_vnet_name-fw --resource-group $rg --nics $site1_vnet_name-fw-wan $site1_vnet_name-fw-lan --size Standard_B2als_v2 --attach-os-disk $diskId --os-type linux
+az vm create --name $site1_vnet_name-fw --resource-group $rg --nics $site1_vnet_name-fw-wan $site1_vnet_name-fw-lan --size Standard_B2als_v2 --attach-os-disk $diskId --os-type linux -o none
 site1_fw_public_ip=$(az network public-ip show -g $rg -n $site1_vnet_name-fw --query 'ipAddress' -o tsv | tr -d '\r') && echo $site1_vnet_name-fw public ip: $site1_fw_public_ip
 site1_fw_wan_private_ip=$(az network nic show -g $rg -n $site1_vnet_name-fw-wan --query ipConfigurations[].privateIPAddress -o tsv | tr -d '\r') && echo $site1_vnet_name-fw wan private IP: $site1_fw_wan_private_ip
 site1_fw_lan_private_ip=$(az network nic show -g $rg -n $site1_vnet_name-fw-lan --query ipConfigurations[].privateIPAddress -o tsv | tr -d '\r') && echo $site1_vnet_name-fw lan private IP: $site1_fw_lan_private_ip
@@ -57,7 +57,7 @@ az network vnet subnet create -g $rg -n $site2_fw_subnet_name --address-prefixes
 
 # create a managed disk from a vhd
 echo -e "\e[1;36mCreating $site2_vnet_name-fw managed disk from a vhd...\e[0m"
-az disk create --resource-group $rg --name $site2_vnet_name-fw --sku $storageType --location $location --size-gb 30 --source $vhdUri --os-type Linux
+az disk create --resource-group $rg --name $site2_vnet_name-fw --sku $storageType --location $location --size-gb 30 --source $vhdUri --os-type Linux -o none
 #Get the resource Id of the managed disk
 diskId=$(az disk show --name $site2_vnet_name-fw --resource-group $rg --query [id] -o tsv | tr -d '\r')
 
@@ -66,7 +66,7 @@ echo -e "\e[1;36mCreating $site2_vnet_name-fw VM...\e[0m"
 az network public-ip create -g $rg -n $site2_vnet_name-fw -l $location --allocation-method Static --sku Basic -o none
 az network nic create -g $rg -n $site2_vnet_name-fw-wan --subnet $site2_fw_subnet_name --vnet-name $site2_vnet_name --ip-forwarding true --private-ip-address 10.2.0.250 --public-ip-address $site2_vnet_name-fw -o none
 az network nic create -g $rg -n $site2_vnet_name-fw-lan --subnet $site2_vm_subnet_name --vnet-name $site2_vnet_name --ip-forwarding true --private-ip-address 10.2.1.250 -o none
-az vm create --name $site2_vnet_name-fw --resource-group $rg --nics $site2_vnet_name-fw-wan $site2_vnet_name-fw-lan --size Standard_B2als_v2 --attach-os-disk $diskId --os-type linux
+az vm create --name $site2_vnet_name-fw --resource-group $rg --nics $site2_vnet_name-fw-wan $site2_vnet_name-fw-lan --size Standard_B2als_v2 --attach-os-disk $diskId --os-type linux -o none
 site2_fw_public_ip=$(az network public-ip show -g $rg -n $site2_vnet_name-fw --query 'ipAddress' -o tsv | tr -d '\r') && echo $site2_vnet_name-fw public ip: $site2_fw_public_ip
 site2_fw_wan_private_ip=$(az network nic show -g $rg -n $site2_vnet_name-fw-wan --query ipConfigurations[].privateIPAddress -o tsv | tr -d '\r') && echo $site2_vnet_name-fw wan private IP: $site2_fw_wan_private_ip
 site2_fw_lan_private_ip=$(az network nic show -g $rg -n $site2_vnet_name-fw-lan --query ipConfigurations[].privateIPAddress -o tsv | tr -d '\r') && echo $site2_vnet_name-fw lan private IP: $site2_fw_lan_private_ip
@@ -87,7 +87,6 @@ az network nic create -g $rg -n "$site2_vnet_name" -l $location --vnet-name $sit
 az vm create -g $rg -n $site2_vnet_name -l $location --image Ubuntu2404 --nics "$site2_vnet_name" --os-disk-name "$site2_vnet_name" --size $vm_size --admin-username $admin_username --admin-password $admin_password --no-wait -o none
 site2_vm_ip=$(az network nic show -g $rg -n $site2_vnet_name --query ipConfigurations[].privateIPAddress -o tsv | tr -d '\r') && echo $site2_vnet_name vm private ip: $site2_vm_ip
 
-
 # site1 route table
 echo -e "\e[1;36mCreating $site1_vnet_name route table....\e[0m"
 az network route-table create -g $rg -n $site1_vnet_name -l $location -o none
@@ -103,6 +102,9 @@ az network vnet subnet update -g $rg -n $site2_vm_subnet_name --vnet-name $site2
 # Follow this documentation to configure pfsense ipsec s2s vpn between the two sites: https://docs.netgate.com/pfsense/en/latest/recipes/ipsec-s2s-psk.html but take the following into account:
 # 1. In phase 1, set 'My identifier'/'Peer identifier' to IP address and put the public ip address of each pfsense firewall
 # 2. in phase 2, set the 'local network'/'remote network' to network and put the $site1_vnet_address and $site2_vnet_address
-# 3. In the IPSec firewall rule, set the 'destination' to 'network' and put the other site vnet address space
+# 3. In the IPSec firewall rule, set the 'destination' to 'network' and out source as other pfsense vnet address space, while destination as the current pfsense vnet address space
+# credentials for pfsense web interface:
+# username: admin
+# password: pfsense
 
 # az group delete -n $rg -y --no-wait
